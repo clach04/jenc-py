da2070eed57c59b790735db8aaf592d02d1276f0
clach04 <clach04@gmail.com>
2024-11-15 18:33:56 -0800

    Error check bad version prefix/magicfile value - partial #6

--- jenc/tests/testsuite.py
+++ jenc/tests/testsuite.py
@@ -59,6 +59,10 @@ class TestUtil(unittest.TestCase):
             #raise Exception(reason)
             """
 
+hello_password = 'geheim'
+hello_world_plaintext = b"Hello World"
+hello_world_v001 = b'V001\xa3\x9d\xf7\xa4\xa7\xa1\xff\x18\x82y\x18\x83\xc6\x97RMgp\xd1\xa6\t\x9c+\xb8\x97\x85U\xed\x02\xca\xaa\xbe[\x92<\xc2\x8cuf\x1b\x03TA&\rkm\x86GH\xd1\xb7\xb5\x1e\x81\xf0\xe4\xa9J\xcei\x130\xe6\xa4\x93\x9aMgh\x9e\xa3\xd3I\xd0a\x98U\x9f6<\x01\xe2A\x88\x9d!\x02\xbe\xf5\xb9\xdd\xb9\xa7\t)N?\x8e\x03K\xb0e?\xd4\x9e\x99\x96\x10R\xf4FC\xe6V\xd7\xdfF\xdbn\xc1bc'
+
 class TestJencUtil(TestUtil):
     def check_get_what_you_put_in(self, original_plaintext, password, version=None, decrypt_password=None):
         decrypt_password = decrypt_password or password
@@ -91,12 +95,19 @@ class TestJencUtil(TestUtil):
         self.assertNotEqual(encrypted_bytes1, encrypted_bytes2)
 
 class TestJencErrors(TestJencUtil):
-    def test_hello_world_enc_dec_default_encryption(self):
+    def test_hello_world_wrong_password(self):
         password = 'geheim'  # same password used in demos for Java version https://github.com/opensource21/jpencconverter/tree/master/src/test/encrypted
         decrypt_password = 'bad password'  # deliberately different
         original_plaintext = b"Hello World"
         self.assertNotEqual(password, decrypt_password)
         self.assertRaises(jenc.JencDecryptError, self.check_get_what_you_put_in, original_plaintext, password, decrypt_password=decrypt_password)
+
+    def test_hello_world_dec(self):
+        encrypted_bytes = jenc.decrypt(hello_password, hello_world_v001)
+        self.assertEqual(encrypted_bytes, hello_world_plaintext)
+        invalid_version = b'AAAA'
+        self.assertRaises(jenc.UnsupportedMetaData, jenc.decrypt, hello_password, invalid_version + hello_world_v001[4:])
+
 
 class TestJenc(TestJencUtil):
 
@@ -190,7 +201,6 @@ class TestJencFiles(TestJencUtil):
     def test_jpencconverter_v001(self):
         self.check_decrypt_file('test.v001_winnewlines.md.jenc', 'test.v001.md')  # NOTE jpencconverter trims off newline at EOF!
 
-# TODO test decryption failure on bad version. e.g. 0000
 # TODO test decryption failure on corrupted file (change a byte, hmac and also payload).
 
 
